0ad19c44cdfa8416b8b7344cd8eb779f179a8b20
simrat39 <simrats169169@gmail.com>
2021-03-17 21:15:33 -0700

    main: Move everything to a run function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
-use std::{error::Error, io::Stdout};
+use std::{
+    error::Error,
+    io::{stdout, Stdout},
+};
 
+use cpu::get_styled_cpu;
 use crossterm::{
     style::{self, Colorize, Print, StyledContent},
     ExecutableCommand, QueueableCommand,
 };
+use de::get_styled_de;
+use kernel::get_styled_kernel;
+use memory::get_styled_memory;
+use name::get_styled_name;
+use os::get_styled_os;
+use shell::get_styled_shell;
+use uptime::get_styled_uptime;
+use wm::get_styled_wm;
 
 pub mod cpu;
 pub mod de;
@@ -16,7 +28,39 @@ pub mod styled_data;
 pub mod uptime;
 pub mod wm;
 
-pub fn print_styled_text(
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let mut out = stdout();
+    let padding: usize = 2;
+
+    let styled_os = get_styled_os();
+    let styled_kernel = get_styled_kernel();
+    let styled_cpu = get_styled_cpu();
+    let styled_name = get_styled_name();
+    let styled_uptime = get_styled_uptime();
+    let styled_de = get_styled_de();
+    let styled_wm = get_styled_wm();
+    let styled_shell = get_styled_shell();
+    let styled_memory = get_styled_memory();
+
+    println!();
+
+    print_styled_text(&mut out, &styled_name.styles, padding)?;
+    print_styled_text(&mut out, &get_styled_line(styled_name.len), padding)?;
+
+    print_styled_text(&mut out, &styled_os.styles, padding)?;
+    print_styled_text(&mut out, &styled_kernel.styles, padding)?;
+    print_styled_text(&mut out, &styled_cpu.styles, padding)?;
+    print_styled_text(&mut out, &styled_uptime.styles, padding)?;
+    print_styled_text(&mut out, &styled_de.styles, padding)?;
+    print_styled_text(&mut out, &styled_wm.styles, padding)?;
+    print_styled_text(&mut out, &styled_shell.styles, padding)?;
+    print_styled_text(&mut out, &styled_memory.styles, padding)?;
+
+    println!();
+    Ok(())
+}
+
+fn print_styled_text(
     out: &mut Stdout,
     vec: &[StyledContent<String>],
     padding: usize,
@@ -31,7 +75,7 @@ pub fn print_styled_text(
 
 // Returns a line as long as the name
 // Something like: ------------------
-pub fn get_styled_line(len: usize) -> Vec<StyledContent<String>> {
+fn get_styled_line(len: usize) -> Vec<StyledContent<String>> {
     let mut ret: Vec<StyledContent<String>> = vec![];
     for _ in 0..len {
         ret.push("-".to_string().white());
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,10 @@
-use std::io::stdout;
+use std::process;
 
-use rustfetch::{
-    cpu::get_styled_cpu, de::get_styled_de, get_styled_line, kernel::get_styled_kernel,
-    memory::get_styled_memory, name::get_styled_name, os::get_styled_os, shell::get_styled_shell,
-    wm::get_styled_wm,
-};
-use rustfetch::{print_styled_text, uptime::get_styled_uptime};
+use rustfetch::run;
 
 fn main() {
-    let mut out = stdout();
-    let padding: usize = 2;
-
-    let styled_os = get_styled_os();
-    let styled_kernel = get_styled_kernel();
-    let styled_cpu = get_styled_cpu();
-    let styled_name = get_styled_name();
-    let styled_uptime = get_styled_uptime();
-    let styled_de = get_styled_de();
-    let styled_wm = get_styled_wm();
-    let styled_shell = get_styled_shell();
-    let styled_memory = get_styled_memory();
-
-    println!();
-
-    print_styled_text(&mut out, &styled_name.styles, padding).unwrap();
-    print_styled_text(&mut out, &get_styled_line(styled_name.len), padding).unwrap();
-
-    print_styled_text(&mut out, &styled_os.styles, padding).unwrap();
-    print_styled_text(&mut out, &styled_kernel.styles, padding).unwrap();
-    print_styled_text(&mut out, &styled_cpu.styles, padding).unwrap();
-    print_styled_text(&mut out, &styled_uptime.styles, padding).unwrap();
-    print_styled_text(&mut out, &styled_de.styles, padding).unwrap();
-    print_styled_text(&mut out, &styled_wm.styles, padding).unwrap();
-    print_styled_text(&mut out, &styled_shell.styles, padding).unwrap();
-    print_styled_text(&mut out, &styled_memory.styles, padding).unwrap();
-
-    println!();
+    run().unwrap_or_else(|err| {
+        eprintln!("An error occured: {}", err);
+        process::exit(1);
+    });
 }
